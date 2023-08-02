3326409bce3ef7ed0f377ebde27a5837f5b7db92
pereznacho <107572987+pereznacho@users.noreply.github.com>
2023-08-01 23:30:15 -0300

    Update serveo.sh
    AutoSSH install included on script

--- serveo.sh
+++ serveo.sh
@@ -5,8 +5,15 @@ BLUE='\033[0;34m'
 BWhite='\033[1;37m'
 NC='\033[0m'
 
-echo -e "${GREEN}Please, choose your Serveo subdomain: "
-read subdom
+if command -v autossh > /dev/null 2>&1; then
+    echo -e "${BLUE}Program exists, keep moving..." 
+    echo -e "${GREEN}Please, choose your Serveo subdomain: "
+    read subdom
 
-echo -e "${BLUE}Starting autossh with your custom SubDomain${NC} ${BWhite}$subdom.serveo.net\n"
-autossh -M 0 -R $subdom.serveo.net:80:localhost:8080 serveo.net
+    echo -e "${BLUE}Starting autossh with your custom SubDomain${NC} ${BWhite}$subdom.serveo.net\n"
+    autossh -M 0 -R $subdom.serveo.net:80:localhost:8080 serveo.net
+else
+    echo -e "${GREEN}Program does not exist. We will install it!"
+    sudo apt install -y autossh > /dev/null 2>&1 
+    echo -e "${BLUE}Please re-run the script again, config your API key and you will be ready to setup your own Serveo Subdomain."
+fi
